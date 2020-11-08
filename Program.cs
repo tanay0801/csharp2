26e3b2e2e911c25c0204cc6eeb6592a270844ff3
TanayDeshmukh <43087469+tanay0801@users.noreply.github.com>
2020-11-08 17:07:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace nullvalue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? TicketOnSale = 500;
+            int Availabletickets;
+
+            if(TicketOnSale==null)
+            {
+                Availabletickets = 0;
+            }
+            else
+            {
+                Availabletickets = (int)TicketOnSale;
+            }
+            Console.WriteLine("Available Tickets = {0}", Availabletickets);
+        }
+    }
+}
